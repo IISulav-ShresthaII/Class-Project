d243031684196fcb7cb537a8b251797965a0e50d
sulavshrestha <sulavshrestha@SULAVSHREST7E8F>
2023-12-25 08:26:52 +0545

    Add project files.

---
+++ asdasd/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace asdasd
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ asdasd/Models/ToDoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asdasd.Models
+{
+    internal class ToDoModel
+    {
+        public int ID { get; set; }
+        public string Task { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public bool Delete { set; get; } = false;
+    }
+}
